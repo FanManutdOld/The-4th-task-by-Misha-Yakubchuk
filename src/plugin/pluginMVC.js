613efd8466ab3faa5e87b8e3b1618471b5ee5386
FanManutd <maykl.yakubchuk@mail.ru>
2020-07-06 18:13:03 +0300

    Started MVC(don't work)

---
+++ src/plugin/pluginMVC.js
@@ -0,0 +1,24 @@
+class Model {
+  constructor (slider, data) {
+    this.slider = slider;
+    this.min = data.min;
+    this.max = data.max;
+    this.current = data.current;
+    this.init();
+  }  
+
+}
+
+class Controller {
+  constructor () {
+    
+  }
+}
+
+class View {
+  constructor () {
+
+  }
+}
+
+export default Model;
