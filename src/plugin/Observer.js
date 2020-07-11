79d9608e0635d6b715a6f3e6daf64c8f9becf4ac
FanManutd <maykl.yakubchuk@mail.ru>
2020-07-11 15:21:08 +0300

    Created Observer.js

---
+++ src/plugin/Observer.js
@@ -0,0 +1,38 @@
+class Observer {
+  constructor() {
+    this.observers = [];
+  }
+
+  addObserver(o) {
+    if (typeof o !== 'function') {
+      throw new Error('observer must be a function');
+    }
+    for (let i = 0, ilen = this.observers.length; i < ilen; i += 1) {
+      let observer = this.observers[i];
+      if (observer === o) {
+        throw new Error('observer already in the list');
+      }
+    }
+    this.observers.push(o);
+  }
+
+  removeObserver(o) {
+    for (let i = 0, ilen = this.observers.length; i < ilen; i += 1) {
+      let observer = this.observers[i];
+      if (observer === o) {
+        observers.splice(i, 1);
+        return;
+      }
+    }
+    throw new Error('could not find observer in list of observers');
+  }
+
+  notifyObservers(data) {
+    let observersSnapshot = this.observers.slice(0);
+    for (let i = 0, ilen = observersSnapshot.length; i < ilen; i += 1) {
+      observersSnapshot[i](data);
+    }
+  }
+}
+
+export default Observer;
