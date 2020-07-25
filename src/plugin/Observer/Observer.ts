55a57176a9664ec21e0141149004b8106e4bd9a6
FanManutd <maykl.yakubchuk@mail.ru>
2020-07-25 14:26:21 +0300

    Removed unused methods in Observer

--- src/plugin/Observer/Observer.ts
+++ src/plugin/Observer/Observer.ts
@@ -17,41 +17,16 @@ class Observer {
     this.observers[eventType].data.push(callback);
   }
 
-  public addObce(eventType: string, callback: Function) {
-    this.add(eventType, callback);
-    this.observers[eventType].isAddOnce = true;
-  }
-
-  public remove(eventType: string, callback: Function) {
-    if (this.observers[eventType]) {
-      if (this.observers[eventType].data.includes(callback)) {
-        this.observers[eventType].data = this.observers[eventType]
-          .data.filter((observer) => observer !== callback);
-        // если наблюдателей больше нет, удаляем свойство из observers.
-        if (this.observers[eventType].data.length === 0) {
-          delete this.observers[eventType];
-        }
-        return;
-      }
-      throw new Error('could not find callback in list of callbacks');
-    }
-    throw new Error('could not find observer in list of observers');
-  }
-
   protected notify(eventType: string, data?: any) {
     if (this.observers[eventType] === undefined || this.observers[eventType].data === undefined) {
       throw new Error('could not find callback or observer');
     }
 
-    const that = this;
-
     // Make a copy of observer list in case the list
     // is mutated during the notifications.
     const observersTemp = this.observers[eventType].data.slice();
+
     observersTemp.forEach((observer: Function) => {
-      if (that.observers[eventType].isAddOnce) {
-        that.remove(eventType, that.observers[eventType].data[0]);
-      }
       observer(data);
     });
   }
