f2c442c0c211fc0c5edd1a44a4166d320aee0ac6
FanManutd <maykl.yakubchuk@mail.ru>
2020-08-05 00:17:15 +0300

    Added more-sliders page

---
+++ more-sliders/jquery-in-parcel.js
@@ -0,0 +1,4 @@
+const jquery = require('../node_modules/jquery/dist/jquery.js');
+
+// eslint-disable-next-line no-multi-assign
+window.$ = window.jQuery = jquery;
---
+++ more-sliders/more-sliders.js
@@ -0,0 +1,240 @@
+import App from '../src/plugin/App/App.ts';
+
+// eslint-disable-next-line no-undef
+$(document).ready(() => {
+  const slider1 = document.querySelector('.more-sliders__plugin1');
+  new App(slider1, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'orange',
+    // isMinMax: false,
+    double: true,
+  });
+  const slider2 = document.querySelector('.more-sliders__plugin2');
+  new App(slider2, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'orange',
+    double: true,
+    vertical: true,
+  });
+  const slider3 = document.querySelector('.more-sliders__plugin3');
+  new App(slider3, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'orange',
+    vertical: true,
+  });
+  const slider4 = document.querySelector('.more-sliders__plugin4');
+  new App(slider4, {
+    min: 2,
+    max: 5,
+    from: 300,
+    to: 3,
+    step: 0.5,
+    scin: 'orange',
+  });
+  const slider5 = document.querySelector('.more-sliders__plugin5');
+  new App(slider5, {
+    min: -1000,
+    max: 1000,
+    double: true,
+    from: -1000,
+    to: 1000,
+    scin: 'orange',
+  });
+  const slider6 = document.querySelector('.more-sliders__plugin6');
+  new App(slider6, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'darkcongo',
+    // isMinMax: false,
+    double: true,
+  });
+  const slider7 = document.querySelector('.more-sliders__plugin7');
+  new App(slider7, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'darkcongo',
+    double: true,
+    vertical: true,
+  });
+  const slider8 = document.querySelector('.more-sliders__plugin8');
+  new App(slider8, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'darkcongo',
+    vertical: true,
+  });
+  const slider9 = document.querySelector('.more-sliders__plugin9');
+  new App(slider9, {
+    min: 2,
+    max: 5,
+    from: 300,
+    to: 3,
+    step: 0.5,
+    scin: 'darkcongo',
+  });
+  const slider10 = document.querySelector('.more-sliders__plugin10');
+  new App(slider10, {
+    min: -1000,
+    max: 1000,
+    double: true,
+    from: -1000,
+    to: 1000,
+    scin: 'darkcongo',
+  });
+  const slider11 = document.querySelector('.more-sliders__plugin11');
+  new App(slider11, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'whitered',
+    isMinMax: false,
+    double: true,
+  });
+  const slider12 = document.querySelector('.more-sliders__plugin12');
+  new App(slider12, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'whitered',
+    double: true,
+    vertical: true,
+  });
+  const slider13 = document.querySelector('.more-sliders__plugin13');
+  new App(slider13, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'whitered',
+    vertical: true,
+  });
+  const slider14 = document.querySelector('.more-sliders__plugin14');
+  new App(slider14, {
+    min: 2,
+    max: 5,
+    from: 300,
+    to: 3,
+    step: 0.5,
+    scin: 'whitered',
+  });
+  const slider15 = document.querySelector('.more-sliders__plugin15');
+  new App(slider15, {
+    min: -1000,
+    max: 1000,
+    double: true,
+    from: -1000,
+    to: 1000,
+    scin: 'whitered',
+  });
+  const slider16 = document.querySelector('.more-sliders__plugin16');
+  new App(slider16, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'azure',
+    // isMinMax: false,
+    double: true,
+  });
+  const slider17 = document.querySelector('.more-sliders__plugin17');
+  new App(slider17, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'azure',
+    double: true,
+    vertical: true,
+  });
+  const slider18 = document.querySelector('.more-sliders__plugin18');
+  new App(slider18, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'azure',
+    vertical: true,
+  });
+  const slider19 = document.querySelector('.more-sliders__plugin19');
+  new App(slider19, {
+    min: 2,
+    max: 5,
+    from: 300,
+    to: 3,
+    step: 0.5,
+    scin: 'azure',
+  });
+  const slider20 = document.querySelector('.more-sliders__plugin20');
+  new App(slider20, {
+    min: -1000,
+    max: 1000,
+    double: true,
+    from: -1000,
+    to: 1000,
+    scin: 'azure',
+  });
+  const slider21 = document.querySelector('.more-sliders__plugin21');
+  new App(slider21, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'indigo',
+    isMinMax: false,
+    double: true,
+  });
+  const slider22 = document.querySelector('.more-sliders__plugin22');
+  new App(slider22, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'indigo',
+    double: true,
+    vertical: true,
+  });
+  const slider23 = document.querySelector('.more-sliders__plugin23');
+  new App(slider23, {
+    min: 0,
+    max: 1000,
+    from: 300,
+    to: 700,
+    scin: 'indigo',
+    vertical: true,
+  });
+  const slider24 = document.querySelector('.more-sliders__plugin24');
+  new App(slider24, {
+    min: 2,
+    max: 5,
+    from: 300,
+    to: 3,
+    step: 0.5,
+    scin: 'indigo',
+  });
+  const slider25 = document.querySelector('.more-sliders__plugin25');
+  new App(slider25, {
+    min: -1000,
+    max: 1000,
+    double: true,
+    from: -1000,
+    to: 1000,
+    scin: 'indigo',
+  });
+});
