93faae4f5e7295c2f09c0b0f761f1b5bacf2414d
FanManutd <maykl.yakubchuk@mail.ru>
2020-08-22 20:46:56 +0300

    Scale in process

--- src/plugin/View/Scale.ts
+++ src/plugin/View/Scale.ts
@@ -1,4 +1,5 @@
-import IConfig from './../IConfig';
+// eslint-disable-next-line no-unused-vars
+import IConfig from '../IConfig';
 
 class Scale {
   private slider: HTMLElement;
@@ -15,7 +16,6 @@ class Scale {
   public update(isScale: boolean, leftEdge: number, rightEdge: number, config: IConfig) {
     if (isScale) {
       this.scale.style.visibility = 'visible';
-      this.drawScale(config);
       if (this.vertical) {
         this.scale.style.bottom = `${(leftEdge / this.slider.offsetHeight) * 100}%`;
         this.scale.style.height = `${((this.slider.offsetHeight - leftEdge - rightEdge) / this.slider.offsetHeight) * 100}%`;
@@ -23,6 +23,7 @@ class Scale {
         this.scale.style.left = `${(leftEdge / this.slider.offsetWidth) * 100}%`;
         this.scale.style.width = `${((this.slider.offsetWidth - leftEdge - rightEdge) / this.slider.offsetWidth) * 100}%`;
       }
+      this.drawScale(config);
     } else {
       this.scale.style.visibility = 'hidden';
     }
@@ -49,19 +50,32 @@ class Scale {
     } = config;
     let stick: HTMLElement; let text: HTMLElement;
     this.slider.removeChild(this.scale);
-    this.scale = document.createElement('div');
-    this.scale.className = 'slider__scale';
+    this.scale.innerHTML = '';
     this.slider.prepend(this.scale);
     stick = document.createElement('div');
     stick.className = 'slider__scale-big';
     stick.style.left = '0%';
     text = document.createElement('div');
     text.className = 'slider__scale-text';
-    text.style.left = '0%';
     text.textContent = `${min}`;
     this.scale.append(stick, text);
+    text.style.left = `${0 - (text.offsetWidth / 2 / this.slider.offsetWidth) * 100}%`;
+    for (let i = 1; i < 20; i++) {
+      stick = document.createElement('div');
+      if (i % 5 === 0) {
+        stick.className = 'slider__scale-big';
+        text = document.createElement('div');
+        text.className = 'slider__scale-text';
+        text.textContent = `${(max - min) / (100 / 20 - 1) * (i / 5)}`;
+        this.scale.append(text);
+        text.style.left = `${i * (100 / 20) - (text.offsetWidth / 2 / this.slider.offsetWidth) * 100}%`;
+      } else {
+        stick.className = 'slider__scale-small';
+      }
+      stick.style.left = `${i * (100 / 20)}%`;
+      this.scale.append(stick);
+    }
 
-    
     stick = document.createElement('div');
     stick.className = 'slider__scale-big';
     stick.style.left = '100%';
@@ -70,7 +84,7 @@ class Scale {
     text.textContent = `${max}`;
     this.scale.append(stick, text);
     text.style.left = `${100 - (text.offsetWidth / 2 / this.slider.offsetWidth) * 100}%`;
-    
+
   }
 }
 
