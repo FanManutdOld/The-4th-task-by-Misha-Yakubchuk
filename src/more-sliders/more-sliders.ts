dd0895d8007ea9dd5918c85b09006dfcb55e3508
FanManutd <maykl.yakubchuk@mail.ru>
2020-08-26 13:55:34 +0300

    Updated more-sliders

--- src/more-sliders/more-sliders.ts
+++ src/more-sliders/more-sliders.ts
@@ -1,19 +1,18 @@
-import App from '../plugin/App/App.ts';
+/* eslint-disable no-undef */
+import '../plugin/jQueryInterface';
+import '../plugin/mySlider';
 
-// eslint-disable-next-line no-undef
 $(document).ready(() => {
-  const slider1 = document.querySelector('.more-sliders__plugin1');
-  new App(slider1, {
+  $('.more-sliders__plugin1').mySlider({
     min: 0,
     max: 1000,
     from: 300,
     to: 700,
     scin: 'orange',
-    // isMinMax: false,
     double: true,
+    minMax: true,
   });
-  const slider2 = document.querySelector('.more-sliders__plugin2');
-  new App(slider2, {
+  $('.more-sliders__plugin2').mySlider({
     min: 0,
     max: 1000,
     from: 300,
@@ -21,46 +20,45 @@ $(document).ready(() => {
     scin: 'orange',
     double: true,
     vertical: true,
+    minMax: true,
   });
-  const slider3 = document.querySelector('.more-sliders__plugin3');
-  new App(slider3, {
+  $('.more-sliders__plugin3').mySlider({
     min: 0,
     max: 1000,
     from: 300,
     to: 700,
     scin: 'orange',
     vertical: true,
+    minMax: true,
   });
-  const slider4 = document.querySelector('.more-sliders__plugin4');
-  new App(slider4, {
+  $('.more-sliders__plugin4').mySlider({
     min: 2,
     max: 5,
     from: 300,
     to: 3,
     step: 0.5,
     scin: 'orange',
+    minMax: true,
   });
-  const slider5 = document.querySelector('.more-sliders__plugin5');
-  new App(slider5, {
+  $('.more-sliders__plugin5').mySlider({
     min: -1000,
     max: 1000,
     double: true,
     from: -1000,
     to: 1000,
     scin: 'orange',
+    minMax: true,
   });
-  const slider6 = document.querySelector('.more-sliders__plugin6');
-  new App(slider6, {
+  $('.more-sliders__plugin6').mySlider({
     min: 0,
     max: 1000,
     from: 300,
     to: 700,
     scin: 'darkcongo',
-    // isMinMax: false,
     double: true,
+    minMax: true,
   });
-  const slider7 = document.querySelector('.more-sliders__plugin7');
-  new App(slider7, {
+  $('.more-sliders__plugin7').mySlider({
     min: 0,
     max: 1000,
     from: 300,
@@ -68,46 +66,44 @@ $(document).ready(() => {
     scin: 'darkcongo',
     double: true,
     vertical: true,
+    minMax: true,
   });
-  const slider8 = document.querySelector('.more-sliders__plugin8');
-  new App(slider8, {
+  $('.more-sliders__plugin8').mySlider({
     min: 0,
     max: 1000,
     from: 300,
     to: 700,
     scin: 'darkcongo',
     vertical: true,
+    minMax: true,
   });
-  const slider9 = document.querySelector('.more-sliders__plugin9');
-  new App(slider9, {
+  $('.more-sliders__plugin9').mySlider({
     min: 2,
     max: 5,
     from: 300,
     to: 3,
     step: 0.5,
     scin: 'darkcongo',
+    minMax: true,
   });
-  const slider10 = document.querySelector('.more-sliders__plugin10');
-  new App(slider10, {
+  $('.more-sliders__plugin10').mySlider({
     min: -1000,
     max: 1000,
     double: true,
     from: -1000,
     to: 1000,
     scin: 'darkcongo',
+    minMax: true,
   });
-  const slider11 = document.querySelector('.more-sliders__plugin11');
-  new App(slider11, {
+  $('.more-sliders__plugin11').mySlider({
     min: 0,
     max: 1000,
     from: 300,
     to: 700,
     scin: 'whitered',
-    isMinMax: false,
     double: true,
   });
-  const slider12 = document.querySelector('.more-sliders__plugin12');
-  new App(slider12, {
+  $('.more-sliders__plugin12').mySlider({
     min: 0,
     max: 1000,
     from: 300,
@@ -115,46 +111,45 @@ $(document).ready(() => {
     scin: 'whitered',
     double: true,
     vertical: true,
+    minMax: true,
   });
-  const slider13 = document.querySelector('.more-sliders__plugin13');
-  new App(slider13, {
+  $('.more-sliders__plugin13').mySlider({
     min: 0,
     max: 1000,
     from: 300,
     to: 700,
     scin: 'whitered',
     vertical: true,
+    minMax: true,
   });
-  const slider14 = document.querySelector('.more-sliders__plugin14');
-  new App(slider14, {
+  $('.more-sliders__plugin14').mySlider({
     min: 2,
     max: 5,
     from: 300,
     to: 3,
     step: 0.5,
     scin: 'whitered',
+    minMax: true,
   });
-  const slider15 = document.querySelector('.more-sliders__plugin15');
-  new App(slider15, {
+  $('.more-sliders__plugin15').mySlider({
     min: -1000,
     max: 1000,
     double: true,
     from: -1000,
     to: 1000,
     scin: 'whitered',
+    minMax: true,
   });
-  const slider16 = document.querySelector('.more-sliders__plugin16');
-  new App(slider16, {
+  $('.more-sliders__plugin16').mySlider({
     min: 0,
     max: 1000,
     from: 300,
     to: 700,
     scin: 'azure',
-    // isMinMax: false,
     double: true,
+    minMax: true,
   });
-  const slider17 = document.querySelector('.more-sliders__plugin17');
-  new App(slider17, {
+  $('.more-sliders__plugin17').mySlider({
     min: 0,
     max: 1000,
     from: 300,
@@ -162,46 +157,44 @@ $(document).ready(() => {
     scin: 'azure',
     double: true,
     vertical: true,
+    minMax: true,
   });
-  const slider18 = document.querySelector('.more-sliders__plugin18');
-  new App(slider18, {
+  $('.more-sliders__plugin18').mySlider({
     min: 0,
     max: 1000,
     from: 300,
     to: 700,
     scin: 'azure',
     vertical: true,
+    minMax: true,
   });
-  const slider19 = document.querySelector('.more-sliders__plugin19');
-  new App(slider19, {
+  $('.more-sliders__plugin19').mySlider({
     min: 2,
     max: 5,
     from: 300,
     to: 3,
     step: 0.5,
     scin: 'azure',
+    minMax: true,
   });
-  const slider20 = document.querySelector('.more-sliders__plugin20');
-  new App(slider20, {
+  $('.more-sliders__plugin20').mySlider({
     min: -1000,
     max: 1000,
     double: true,
     from: -1000,
     to: 1000,
     scin: 'azure',
+    minMax: true,
   });
-  const slider21 = document.querySelector('.more-sliders__plugin21');
-  new App(slider21, {
+  $('.more-sliders__plugin21').mySlider({
     min: 0,
     max: 1000,
     from: 300,
     to: 700,
     scin: 'indigo',
-    isMinMax: false,
     double: true,
   });
-  const slider22 = document.querySelector('.more-sliders__plugin22');
-  new App(slider22, {
+  $('.more-sliders__plugin22').mySlider({
     min: 0,
     max: 1000,
     from: 300,
@@ -209,32 +202,33 @@ $(document).ready(() => {
     scin: 'indigo',
     double: true,
     vertical: true,
+    minMax: true,
   });
-  const slider23 = document.querySelector('.more-sliders__plugin23');
-  new App(slider23, {
+  $('.more-sliders__plugin23').mySlider({
     min: 0,
     max: 1000,
     from: 300,
     to: 700,
     scin: 'indigo',
     vertical: true,
+    minMax: true,
   });
-  const slider24 = document.querySelector('.more-sliders__plugin24');
-  new App(slider24, {
+  $('.more-sliders__plugin24').mySlider({
     min: 2,
     max: 5,
     from: 300,
     to: 3,
     step: 0.5,
     scin: 'indigo',
+    minMax: true,
   });
-  const slider25 = document.querySelector('.more-sliders__plugin25');
-  new App(slider25, {
+  $('.more-sliders__plugin25').mySlider({
     min: -1000,
     max: 1000,
     double: true,
     from: -1000,
     to: 1000,
     scin: 'indigo',
+    minMax: true,
   });
 });
