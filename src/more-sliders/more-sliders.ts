f5d0f06dba5c65b2f9a2b4348dbc998dc0f1746d
FanManutd <maykl.yakubchuk@mail.ru>
2021-08-11 17:24:47 +0300

    Added slider-example component to more-sliders #9

--- src/more-sliders/more-sliders.ts
+++ src/more-sliders/more-sliders.ts
@@ -1,6 +1,9 @@
 import '../plugin/mySlider';
+import SliderExample from '../demo-page/components/slider-example/slider-example';
 
 $(document).ready(() => {
-  const sliders = $('.js-more-sliders__slider');
-  sliders.mySlider();
+  $('.js-more-sliders__slider').each((_, element) => {
+    const slider = new SliderExample(element);
+    slider.addSlider();
+  });
 });
